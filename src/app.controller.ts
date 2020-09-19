2cb9ab8585c7b85858ce7f2f8ccf2a081cb1230b
Josue <josue.quilon-barrios@senecacollege.ca>
2020-09-19 02:18:45 -0400

    feat: parse json with user information

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Post, Body, Logger } from '@nestjs/common';
+import { join } from 'path';
+import { promises as fs } from 'fs';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-}
+  @Post('eod_test')
+  async getStatus(@Body() body: { user_name: string }): Promise<string> {
+    const logger = new Logger('eodStatus');
+    const { user_name } = body;
+
+    const json = await fs.readFile(join(__dirname, '../config_files/status.json'), 'utf8');
+    logger.debug(json);
+    logger.debug(`EOD status requested by: ${user_name}`);
+    return "hello world";
+  };
+};
