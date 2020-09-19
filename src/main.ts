e11f1e1f23c1664d183100f52454c0dd212bf735
Josue <josue.quilon-barrios@senecacollege.ca>
2020-09-19 02:17:12 -0400

    feat: add logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LogLevel, Logger } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const port = process.env['PORT'] || '8080';
+const debug: string | undefined = process.env['LOG_DEBUG'];
+
+const levels: LogLevel[] = ['error', 'warn', 'verbose', 'log'];
+if (debug) levels.push('debug');
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: levels,
+  });
+  const logger = new Logger();
+
+  logger.log(`Osteppy server listening on port ${port}`);
+  await app.listen(port);
 }
 bootstrap();
